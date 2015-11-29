da50686da41284891fac77acb1984166977fec53
Ethan Henderson <ethan@zbee.me>
2015-11-29 13:32:31 -0700

    Separated theme

---
+++ assets/php/header.php
@@ -0,0 +1,46 @@
+<?php
+require_once(__DIR__ . "/config.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <title>Are You Awake?</title>
+    <meta name="generator" content="Bootply" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+      <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <link href="/assets/css/styles.css" rel="stylesheet">
+  </head>
+  <body>
+<nav class="navbar navbar-trans navbar-fixed-top" role="navigation">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapsible">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="/">Are You Awake?</a>
+    </div>
+    <div class="navbar-collapse collapse" id="navbar-collapsible">
+      <ul class="nav navbar-nav navbar-left">
+        <li><a href="/#section2">What?</a></li>
+        <li><a href="/#section3">Why?</a></li>
+        <li><a href="/#section4">How?</a></li>
+        <li><a href="/#section6">Who?</a></li>
+        <li>&nbsp;</li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="/#section5">Log In</a></li>
+        <li>&nbsp;</li>
+      </ul>
+    </div>
+  </div>
+</nav>

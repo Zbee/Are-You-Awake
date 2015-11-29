dd88bdc73c0293bca41f0ab2c229d630ea86b6d0
Ethan Henderson <ethan@zbee.me>
2015-11-29 14:14:31 -0700

    Base usage page (preliminary)

--- use/index.php
+++ use/index.php
@@ -5,16 +5,12 @@ require("/var/www/awake/assets/php/header.php");
 <section class="container-fluid section" id="section6">
   <div class="row">
     <div class="col-sm-8 col-sm-offset-2 text-center">
-        <h1>Who made Are You Awake?</h1>
-        <br>
-        <p class="lead">
-          Are You Awake was first created by <a href="https://keybase.io/Zbee"
-            target="_blank">Ethan Henderson (Zbee)</a> on an off-day.
-        </p>
-        <br>
-        <a href="https://keybase.io/Zbee" target="_blank">
-          <img src="https://i.imgur.com/mGOEBGi.jpg" alt="Zbee's face"
-            class="img-circle" height="256"></a>
+<?php
+$userID = $fb->get('/me')->getDecodedBody()["id"];
+
+$response = $fb->get('/' . $userID)->getDecodedBody();
+var_dump($response);
+?>
     </div>
   </div>
 </section>

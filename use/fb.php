4b5af873c2c45df6a763696ea6987a40d150a3e9
Ethan Henderson <ethan@zbee.me>
2015-11-29 17:10:47 -0700

    Returns more results

--- use/fb.php
+++ use/fb.php
@@ -5,8 +5,7 @@ header('Content-Type: application/json');
 $echo = [];
 $friends = $fb->get('/me/taggable_friends?limit=500')->getDecodedBody()["data"];
 foreach ($friends as $friend)
-  if ($friend["picture"]["data"]["is_silhouette"] == false)
-    array_push($echo, [$friend["name"], $friend["picture"]["data"]["url"]]);
+  array_push($echo, [$friend["name"], $friend["picture"]["data"]["url"]]);
 
 echo json_encode($echo);
 ?>
